a825ee7dc63730964cf6988fa28e5572cd5ae568
Augusto Bildhauer <augustobildhauer@gmail.com>
2026-06-30 20:33:14 -0300

    feat: classe Triangulo com atributos e método area()

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.triangulo;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,33 +10,33 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+
+        triangulo x, y;
+        x = new triangulo();
+        y = new triangulo();
 
         System.out.println("Entre com as medidas do triângulo A: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Entre com as medidas do triângulo B: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double pX = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(pX * (pX - xA) * (pX- xB) * (pX - xC));
-
-        pX = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(pX * (pX - yA) * (pX - yB) * (pX - yC));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Área do triângulo X: %.4f%n", areaX);
         System.out.printf("Área do triângulo Y: %.4f%n", areaY);
         System.out.println();
 
         if (areaX > areaY) {
-            System.out.println("Área maior triângulo: " + areaX);
+            System.out.println("Área maior triângulo: X");
         }
         else {
-            System.out.println("Área maior triângulo: " + areaY);
+            System.out.println("Área maior triângulo: Y");
 
         }
 
